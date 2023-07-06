18dd28ad012f1bf33a6397f9d557b5acf9fa502d
qqmoar <59869715+qqmoar@users.noreply.github.com>
2023-07-06 10:24:49 +0300

    Update get insight attribute.groovy

--- get insight attribute.groovy
+++ get insight attribute.groovy
@@ -1,14 +1,27 @@
 import com.atlassian.jira.component.ComponentAccessor
 
 Class objectFacadeClass = ComponentAccessor.getPluginAccessor().getClassLoader().findClass("com.riadalabs.jira.plugins.insight.channel.external.api.facade.ObjectFacade")
-Class objectAttributeBeanValueClass = ComponentAccessor.getPluginAccessor().getClassLoader().findClass("com.riadalabs.jira.plugins.insight.services.model.ObjectAttributeValueBean")
-def objectFacade = ComponentAccessor.getOSGiComponentInstanceOfType(objectFacadeClass)
-def objectAttributeBeanValue = ComponentAccessor.getOSGiComponentInstanceOfType(objectAttributeBeanValueClass)
 
-String objectKey = "TEST-719"
+objectFacade = ComponentAccessor.getOSGiComponentInstanceOfType(objectFacadeClass)
+
+
+String objectKey = "TEST-733"
 def insightObject = objectFacade.loadObjectBean(objectKey)
 
+//attribute = objectFacade.loadObjectAttributeBean(insightObject.id, 850).getObjectAttributeValueBeans()[0].getValue()
 
-attribute = objectFacade.loadObjectAttributeBean(719, 850)
+getDeveloper(insightObject)
 
-attribute.getObjectAttributeValueBeans()[0].getValue()
+def getDeveloper(insightObject) {
+  def developerKey = objectFacade.loadObjectAttributeBean(insightObject.id, 850).getObjectAttributeValueBeans()?.first()?.getValue()
+  return developerKey
+}
+
+def getAnalytic(insightObject) {
+  def analyticKey = objectFacade.loadObjectAttributeBean(insightObject.id, 851).getObjectAttributeValueBeans()?.first()?.getValue()
+  return analyticKey
+}
+
+
+
+//  есть проблема, что некоторых юзеров получаешь с их usernamе, а какие-то через JIRAUSER9999
