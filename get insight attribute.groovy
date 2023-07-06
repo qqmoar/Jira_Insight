483588cfe2c7287080014e91ffd0bae53af57815
qqmoar <59869715+qqmoar@users.noreply.github.com>
2023-07-06 10:49:27 +0300

    Update get insight attribute.groovy

--- get insight attribute.groovy
+++ get insight attribute.groovy
@@ -1,22 +1,27 @@
 import com.atlassian.jira.component.ComponentAccessor
 
+//	подключаем классы инсайта
 Class objectFacadeClass = ComponentAccessor.getPluginAccessor().getClassLoader().findClass("com.riadalabs.jira.plugins.insight.channel.external.api.facade.ObjectFacade")
 
+//	объект класса
 objectFacade = ComponentAccessor.getOSGiComponentInstanceOfType(objectFacadeClass)
 
-
-String objectKey = "TEST-733"
+//	ключ объекта
+String objectKey = "TEST-719"
+//	получаем объект в виде инсайт корзины
 def insightObject = objectFacade.loadObjectBean(objectKey)
 
-//attribute = objectFacade.loadObjectAttributeBean(insightObject.id, 850).getObjectAttributeValueBeans()[0].getValue()
+getAttributeValue(insightObject, 850)
 
-getAttributeValue(insightObjectб 850)
-
+//
+//	функции
+//
 def getAttributeValue(insightObject, int attributeId) {
   //  получаем значение аттрибута через айди объекта и айди аттрибута
   def attributeVal = objectFacade.loadObjectAttributeBean(insightObject.id, attributeId).getObjectAttributeValueBeans()?.first()?.getValue()
   return attributeVal
 }
+
 
 
 //  есть проблема при аттрибуте с пользаками, потмоу что некоторых юзеров получаешь с их usernamе, а какие-то в виде JIRAUSER9999
